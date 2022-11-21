c96bd139df474c44c883b2f3f1f3887f49dd66d2
KionAoki Wang <kionaokiwang@KionAokideMacBook-Pro.local>
2022-11-21 14:15:47 +0800

    Create login scope

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,29 @@
 <template>
-  <h1>請登入帳號</h1>
   <div>
-    <button>使用帳密登入</button>
+    <h1>請登入帳號</h1>
+    <div>
+      <p>請輸入帳號</p>
+      <input v-model="ancount" />
+      <p>請輸入密碼</p>
+      <input type="password" v-model="password" />
+    </div>
+    <div class="top">
+      <button>送出</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Login",
+  data() {
+    return {};
+  },
   methods: {},
 };
 </script>
+<style scoped>
+.top {
+  margin: 10px 0px;
+}
+</style>
