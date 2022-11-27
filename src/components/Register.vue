906540f485fb4f138489928266d4d70f56819b96
KionAoki Wang <kionaokiwang@KionAokideMacBook-Pro.local>
2022-11-27 11:52:42 +0800

    Add Register component

---
+++ src/components/Register.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>請註冊帳號</h1>
+    <div>
+      <p>請輸入帳號</p>
+      <input v-model="ancount" />
+      <p>請輸入密碼</p>
+      <input type="password" v-model="password" />
+    </div>
+    <div class="top">
+      <button>送出</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+};
+</script>
+
+<style scoped>
+.top {
+  margin: 10px 0px;
+}
+</style>
